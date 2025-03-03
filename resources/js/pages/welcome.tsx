ef218fa32a8dbe039145b9ce44972ba99e202cb8
--replace-all <--replace-all>
2025-03-04 00:54:11 +0100

    deploy(laravel cloud): up up to the sky once again and fix mobile view...

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -74,7 +74,7 @@ export default function Welcome({ chatHistory = [] }) {
 
             <div className="min-h-screen w-full flex flex-col items-center justify-start bg-[#E8F1F2] p-4 pt-8">
                 <div className="w-full max-w-2xl flex flex-col items-center">
-                    <div className="w-full flex flex-col items-center justify-center md:-mt-10 mb-10">
+                    <div className="w-full flex flex-col items-center justify-center -mt-10 mb-10">
                         <div className="flex flex-col md:relative md:w-80 md:h-60">
                             <div className="hidden md:block w-full max-w-xs mx-auto mb-2 md:mb-0 md:absolute md:w-50 md:top-15 md:-left-30 md:z-10">
                                 <video
@@ -87,7 +87,7 @@ export default function Welcome({ chatHistory = [] }) {
                                 />
                             </div>
 
-                            <div className="text-center md:text-left font-['Knewave'] text-5xl md:text-6xl text-[#001A23] md:absolute md:left-10 md:top-12 md:z-0 leading-tight">
+                            <div className="hidden md:block text-center md:text-left font-['Knewave'] text-5xl md:text-6xl text-[#001A23] md:absolute md:left-10 md:top-12 md:z-0 leading-tight">
                                 DAMIAN<br/>ERMANNI
                             </div>
                         </div>
